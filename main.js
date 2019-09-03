144624e6157a0b819515ba5208a230bab5599a4f
Klemek <Klemek@outlook.fr>
2019-09-03 09:34:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 $.ajaxPrefilter(function (options) {
   if (options.crossDomain && jQuery.support.cors) {
     const http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
-    //options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-    options.url = "http://cors.corsproxy.io/url=" + options.url;
+    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
     options.data = options.data.replace(/%2B/gm, '+');
   }
 });
